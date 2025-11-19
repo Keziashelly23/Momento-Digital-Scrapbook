ee6d314828e5fae2cbe1c0d79167a86a0ac4bd8b
Kezia Shelin <Keziashelin11@gmail.com>
2025-11-18 19:24:17 -0500

    fixed color for pages

--- canvas.js
+++ canvas.js
@@ -809,12 +809,16 @@ let colorPicker = new iro.ColorPicker("#colorWheel", {
 });
 
 // Update page color
-colorPicker.on("color:change", color => {
-  leftPageEl.style.backgroundColor = color.hexString;
-  rightPageEl.style.backgroundColor = color.hexString;
+colorPicker.on("color:change", (color) => {
+  // make sure these match your actual page element variables
+  leftPage.style.backgroundColor = color.hexString;
+  rightPage.style.backgroundColor = color.hexString;
 
-  leftPageEl.style.backgroundBlendMode = "multiply";
-  rightPageEl.style.backgroundBlendMode = "multiply";
+  leftPage.style.backgroundBlendMode = "multiply";
+  rightPage.style.backgroundBlendMode = "multiply";
+
+  // schedule autosave so color is remembered
+  scheduleAutoSave();
 });
 
 // Close popup when clicking elsewhere
@@ -823,6 +827,7 @@ document.addEventListener("click", (e) => {
     colorWheelPopup.style.display = "none";
   }
 });
+
 
 /* -------------------------
    Page navigation
